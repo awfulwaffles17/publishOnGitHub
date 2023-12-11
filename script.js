7a2ca86885886ade067d6920cea5107ac6c09e7e
awfulwaffles17 <arundhathymv2@gmail.com>
2023-12-12 04:52:53 +0530

    mouse cursor

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-document.body.onmousemove = function(e) {
-    document.documentElement.style.setProperty (
-      '--x', (
-        e.clientX+window.scrollX
-      )
-      + 'px'
-    );
-    document.documentElement.style.setProperty (
-      '--y', (
-        e.clientY+window.scrollY
-      ) 
-      + 'px'
-    );
-  }
+document.addEventListener('DOMContentLoaded', function () {
+    const bigBall = document.querySelector('.cursor__ball--big');
+    const smallBall = document.querySelector('.cursor__ball--small');
+    const hoverables = document.querySelectorAll('.hoverable');
+  
+    // Listeners
+    document.body.addEventListener('mousemove', onMouseMove);
+    hoverables.forEach(hoverable => {
+      hoverable.addEventListener('mouseenter', onMouseHover);
+      hoverable.addEventListener('mouseleave', onMouseHoverOut);
+    });
+  
+    // Move the cursor
+    function onMouseMove(e) {
+      bigBall.style.transform = `translate3d(${e.pageX - 15}px, ${e.pageY - 15}px, 0)`;
+      smallBall.style.transform = `translate3d(${e.pageX - 5}px, ${e.pageY - 7}px, 0)`;
+    }
+  
+    // Hover an element
+    function onMouseHover() {
+      bigBall.style.transform = 'scale(4)';
+    }
+  
+    function onMouseHoverOut() {
+      bigBall.style.transform = 'scale(1)';
+    }
+  });
