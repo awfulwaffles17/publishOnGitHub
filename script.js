b5a432344bbcbe258bf6067a362bd036369bb982
awfulwaffles17 <arundhathymv2@gmail.com>
2023-12-13 21:13:47 +0530

    commit

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const bigBall = document.querySelector('.cursor__ball--big');
-    const smallBall = document.querySelector('.cursor__ball--small');
-    const hoverables = document.querySelectorAll('.hoverable');
-  
-    // Listeners
-    document.body.addEventListener('mousemove', onMouseMove);
-    hoverables.forEach(hoverable => {
-      hoverable.addEventListener('mouseenter', onMouseHover);
-      hoverable.addEventListener('mouseleave', onMouseHoverOut);
+$(document).ready(function() {
+    $(document).on('mousemove', function(e) {
+        $('#cursor').css({
+            left: e.pageX,
+            top: e.pageY
+        });
     });
-  
-    // Move the cursor
-    function onMouseMove(e) {
-      bigBall.style.transform = `translate3d(${e.pageX - 15}px, ${e.pageY - 15}px, 0)`;
-      smallBall.style.transform = `translate3d(${e.pageX - 5}px, ${e.pageY - 7}px, 0)`;
-    }
-  
-    // Hover an element
-    function onMouseHover() {
-      bigBall.style.transform = 'scale(4)';
-    }
-  
-    function onMouseHoverOut() {
-      bigBall.style.transform = 'scale(1)';
-    }
-  });
+});
+
